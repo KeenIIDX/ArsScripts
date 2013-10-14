d4f35087bc1775f43bfd5d2d0b2a04a9c73170c0
Elliott Wilcoxon <elliott@wilcoxon.org>
2013-10-14 15:55:08 -0500

    Improve previous commit
    I realized there's a slicker way to do this with jquery.

--- Unparticipate_In_Threads.user.js
+++ Unparticipate_In_Threads.user.js
@@ -105,17 +105,7 @@ with_jquery(function($) {
 		threadsSelector = threadsSelector.slice(0,-1);
 		
 		// Grab list of threads.  For each of the ones that should be shown as unposted, hide.
-		$(threadsSelector).each(function () {
-			if (this.src === imgNormalReadPosted) {
-				$(this).parents('tr').remove();
-			} else if (this.src === imgNormalUnreadPosted) {
-				$(this).parents('tr').remove();
-			} else if (this.src === imgStickyReadPosted) {
-				$(this).parents('tr').remove();
-			} else if (this.src === imgStickyUnreadPosted) {
-				$(this).parents('tr').remove();
-			}
-		});
+		$(threadsSelector).parents('tr').remove();
 	}
 	
 	if (document.URL.indexOf("viewtopic") !== -1) {
